3b8157e899b4afc25272bbaa160cbac8c7a5f556
Raz <79261812+razuwaikin@users.noreply.github.com>
2021-06-07 23:28:27 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,47 +3,47 @@
 
 #include "gtest/gtest.h"
 
-TEST1(Account, test1)
+TEST(Account, test1)
 {
 	Account A(1, 5);
 	EXPECT_EQ(A.GetBalance(), 5);
 }
-TEST2(Account, test2)
+TEST(Account, test2)
 {
 	Account A(1, 5);
 	A.Lock();
 	A.ChangeBalance(3);
 	EXPECT_EQ(A.GetBalance(), 8);
 }
-TEST3(Account, test3)
+TEST(Account, test3)
 {
 	Account A(1, 5);
 	EXPECT_ANY_THROW(A.ChangeBalance(3));
 }
-TEST4(Account, test4)
+TEST(Account, test4)
 {
 	Account A(1, 5);
 	A.Lock();
 	EXPECT_ANY_THROW(A.Lock());
 }
 
-TEST5(Account, test5)
+TEST(Account, test5)
 {
 	Account A(1, 5);
 	EXPECT_ANY_THROW(A.ChangeBalance(3));
 }
-TEST6(Transaction, test1)
+TEST(Transaction, test1)
 {
 	Transaction B;
 	EXPECT_EQ(B.fee(), 1);
 }
-TEST7(Transaction, test2)
+TEST(Transaction, test2)
 {
 	Transaction B;
 	B.set_fee(5);
 	EXPECT_EQ(B.fee(), 5);
 }
-TEST8(Transaction, test3)
+TEST(Transaction, test3)
 {
 	Transaction B;
 	Account A1(1, 2000);
@@ -53,7 +53,7 @@ TEST8(Transaction, test3)
 	EXPECT_EQ(A1.GetBalance(), 1500);
 	EXPECT_EQ(A2.GetBalance(), 600);
 }
-TEST9(Transaction, test4)
+TEST(Transaction, test4)
 {
 	Transaction B;
 	Account A1(1, 1000);
@@ -61,7 +61,7 @@ TEST9(Transaction, test4)
 	EXPECT_ANY_THROW(B.Make(A1, A2, -400));
 	EXPECT_ANY_THROW(B.Make(A1, A2, 60));
 }
-TEST10(Transaction, test5)
+TEST(Transaction, test5)
 {
 	Transaction B;
 	Account A1(1, 1000);
@@ -69,14 +69,14 @@ TEST10(Transaction, test5)
 	B.set_fee(300);
 	EXPECT_FALSE(B.Make(A1, A2, 400));
 }
-TEST11(Transaction, test6)
+TEST(Transaction, test6)
 {
 	Transaction B;
 	Account A1(1, 1000);
 	Account A2(2, 200);
 	EXPECT_ANY_THROW(B.Make(A1, A2, -400));
 }
-TEST12(Transaction, test7)
+TEST(Transaction, test7)
 {
 	Transaction B;
 	Account A1(1, 1000);
