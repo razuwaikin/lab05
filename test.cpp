98686deb1ee6446f0f07d92b82503f55160d67b1
Raz <79261812+razuwaikin@users.noreply.github.com>
2021-06-11 21:09:40 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -81,7 +81,7 @@ TEST(Transaction, test7)
 	Account A1(1, 1000);
 	EXPECT_ANY_THROW(B.Make(A1, A1, 400));
 }
-TEST(Transaction,test8){
+TEST(Transaction, test8){
 	Transaction B;
 	Account A1(1,1000);
 	EXPECT_FALSE(B.Debit(A1, 10000));
